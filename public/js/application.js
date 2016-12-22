28607176372f61c4f533b4ba8cf568d7dea9f67b
mal-wood <mallorywood413@gmail.com>
2016-12-22 15:04:21 -0800

    Fix font family

--- public/js/application.js
+++ public/js/application.js
@@ -3,9 +3,10 @@ $(document).ready(function() {
   var normalFontSize = $('.navbar-text').css("font-size");
 
   $('.navbar-text').hover(function(){
-    $(this).css("font-size", "1.2em");
+    $(this).css("font-size", "1.1em");
   }, function() {
     $(this).css("font-size", normalFontSize);
   }
   )
+
 });
