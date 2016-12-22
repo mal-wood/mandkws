2f03120b319eba8af87bd4bf537a45d9db762352
mal-wood <mallorywood413@gmail.com>
2016-12-22 08:50:37 -0800

    Fix navigation jquery

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
 
-  var normalFontsize = $('.navbar-text').css("font-size");
+  var normalFontSize = $('.navbar-text').css("font-size");
 
   $('.navbar-text').hover(function(){
     $(this).css("font-size", "1.2em");
-    $(this).next().css("color", "#EBE4E0");
   }, function() {
-    $(this).css("font-size", normalFontsize)
+    $(this).css("font-size", normalFontSize);
   }
   )
 });
