af746eb4f4f6fd1918642fa7a1a7b2f0b8009a7b
mal-wood <mallorywood413@gmail.com>
2016-12-21 22:35:37 -0800

    Fix navigation bar

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  
+
+  var normalFontsize = $('.navbar-text').css("font-size");
+
+  $('.navbar-text').hover(function(){
+    $(this).css("font-size", "1.2em");
+    $(this).next().css("color", "#EBE4E0");
+  }, function() {
+    $(this).css("font-size", normalFontsize)
+  }
+  )
 });
