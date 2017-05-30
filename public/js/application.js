74022941426e525791d7a0ec01712eedf52479af
mal-wood <mallorywood413@gmail.com>
2017-05-29 22:47:21 -0700

    new changes

--- public/js/application.js
+++ public/js/application.js
@@ -9,4 +9,39 @@ $(document).ready(function() {
     }
   )
 
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFsd29vZCIsImEiOiJjajB5ODExNDgwMjczMzNvMnRtbGJscDRtIn0.Ua3fOk4G2fq_mW3jsGSa4g';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/malwood/cj36uyo2x000c2rpip6quva1a', //stylesheet location
+    center: [-122.126946, 37.852229], // starting position
+    zoom: 11 // starting zoom
+});
+
+map.scrollZoom.disable();
+map.addControl(new mapboxgl.NavigationControl());
+
+    // Create a popup, but don't add it to the map yet.
+    var popup = new mapboxgl.Popup({
+        closeButton: true,
+        closeOnClick: true
+    });
+
+
+map.on('load', function(){
+      map.on('mouseenter', 'wedding-spots', function(e) {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup.setLngLat(e.features[0].geometry.coordinates)
+            .setHTML("<b>" + e.features[0].properties.name + "</b>" + "<br>" + e.features[0].properties.address)
+            .addTo(map);
+    });
+
+    map.on('mouseleave', 'wedding-spots', function() {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    });
+})
 });
